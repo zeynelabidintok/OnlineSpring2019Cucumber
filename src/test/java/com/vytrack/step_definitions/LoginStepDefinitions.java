bff150644eb62d21dd7f42b74fa2e26885bc8b73
zeynel abidin <z.abidintok@gmail.com>
2019-09-05 02:45:16 +0200

    added feature file

--- src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -1,4 +1,34 @@
 package com.vytrack.step_definitions;
 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class LoginStepDefinitions {
+    @Given("Open Vytrack login page")
+    public void open_Vytrack_login_page() {
+
+    }
+
+    @When("Enter valid username and invalid password information")
+    public void enter_valid_username_and_invalid_password_information() {
+
+    }
+
+    @When("Click login")
+    public void click_login() {
+
+    }
+
+    @Then("Message Invalid user name or password. should be displayed")
+    public void message_Invalid_user_name_or_password_should_be_displayed() {
+
+    }
+
+    @Then("Page title and url should be same")
+    public void page_title_and_url_should_be_same() {
+
+    }
+
+
 }
